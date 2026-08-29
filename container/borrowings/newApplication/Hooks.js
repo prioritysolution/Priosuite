4bebfbf84f9b25e44835f4e7c767cbf040439bed
TanmayRana <tanmayrana2001@gmail.com>
2026-08-29 11:05:58 +0530

    Account no fix

--- container/borrowings/newApplication/Hooks.js
+++ container/borrowings/newApplication/Hooks.js
@@ -76,15 +76,15 @@ export const useBorrowingsNewApplication = () => {
       .test("is-valid-integer", "Invalid Account no.", (value) => {
         if (!value) return false; // If the value is empty, it's invalid
         return positiveIntegerRegex.test(value); // Validate using regex (not starting with 0, and integer up to 12 digits)
-      })
-      .test(
-        "is-within-length",
-        "Account no. must be up to 12 digits",
-        (value) => {
-          if (!value) return false; // If the value is empty, it's invalid
-          return value.length <= 12; // Validate that the length is less than or equal to 12
-        },
-      ),
+      }),
+      // .test(
+      //   "is-within-length",
+      //   "Account no. must be up to 12 digits",
+      //   (value) => {
+      //     if (!value) return false; // If the value is empty, it's invalid
+      //     return value.length <= 12; // Validate that the length is less than or equal to 12
+      //   },
+      // ),
     issueDate: yup
       .date()
       .transform((value, originalValue) =>
